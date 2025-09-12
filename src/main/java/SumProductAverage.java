de91097315635a0487fe2498938d7c203c42bf89
JACOB STASIENKO <5caeb93e-5633-49f8-9acb-6064dbc85247@KAM-CA-H4TG50EEQ6X7.local>
2025-09-12 13:49:47 -0400

    sum average product program

--- src/main/java/SumProductAverage.java
+++ src/main/java/SumProductAverage.java
@@ -15,8 +15,33 @@
 
 import java.util.Scanner;
 
-public class SumProductAverage {
-    public static void main(String[] args) {
-        // TODO
+public class SumProductAverage
+{
+    public static void main(String[] args)
+    {
+        // declare variables and scanner
+        int numRuns;
+        double num1, num2 num3, sum, prod, avg;
+        Scanner sc = new Scanner(System.in);
+
+        // prompt for number of runs and loop
+        System.out.println("Input number of runs:  ");
+        numRuns = sc.nextInt();
+        for(int j=0; j<numRuns; j++)
+        {
+            // read in inputs
+            num1 = sc.nextDouble();
+            num2 = sc.nextDouble();
+            num3 = sc.nextDouble();
+
+            // calculate results
+            sum = num1 + num2 + num3;
+            prod = num1 * num2 * num3;
+            avg = sum/3;
+
+            System.out.println("Sum: " + sum);
+            System.out.println("Product: " + prod);
+            System.out.println("Average: " + avg);
+        }
     }
 }
