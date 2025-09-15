5bfe981e0a8af86780ffb334ac228e4838b52db4
goosemanguyperson <jacobstasienko3@gmail.com>
2025-09-15 13:54:16 -0400

    ...and again

--- src/main/java/SumProductAverage.java
+++ src/main/java/SumProductAverage.java
@@ -15,37 +15,34 @@
 
 import java.util.Scanner;
 
-public class SumProductAverage {
+public class SumProductAverage
+{
+    public static void main (String[] args)
     {
-        public static void main (String[]args)
-        {
+        // declare variables
+        int numRuns;
+        double num1, num2, num3, sum, avg, prod;
+        Scanner scan = new Scanner(System.in);
 
+        // prompt user for number of runs, then loop program that many times
+        System.out.print("Input number of runs:  ");
+        numRuns = scan.nextInt();
 
-            // declare variables
-            int numRuns;
-            double num1, num2, num3, sum, avg, prod;
-            Scanner scan = new Scanner(System.in);
+        for (int j = 0; j < numRuns; j++) {
+            // read in user inputs
+            System.out.print("\nInput three numbers:  ");
+            num1 = scan.nextDouble();
+            num2 = scan.nextDouble();
+            num3 = scan.nextDouble();
 
-            // prompt user for number of runs, then loop program that many times
-            System.out.print("Input number of runs:  ");
-            numRuns = scan.nextInt();
+            // calculate the sum, average, and product, then output the results
+            sum = num1 + num2 + num3;
+            avg = sum / 3;
+            prod = num1 * num2 * num3;
 
-            for (int j = 0; j < numRuns; j++) {
-                // read in user inputs
-                System.out.print("\nInput three numbers:  ");
-                num1 = scan.nextDouble();
-                num2 = scan.nextDouble();
-                num3 = scan.nextDouble();
-
-                // calculate the sum, average, and product, then output the results
-                sum = num1 + num2 + num3;
-                avg = sum / 3;
-                prod = num1 * num2 * num3;
-
-                System.out.println(sum);
-                System.out.println(avg);
-                System.out.println(prod);
-            }
+            System.out.println("Sum: " + sum);
+            System.out.println("Average: " + avg);
+            System.out.println("Product: " + prod);
         }
     }
 }
